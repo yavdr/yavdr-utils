0aab627b6b2e8b065a527011cf5d5b193e3b96a6
Marco Scholl <develop@marco-scholl.de>
2011-02-28 23:31:47 +0100

    remove wrong dir

--- yavdr-utils/db-utils/dbget.h
+++
@@ -1,6 +0,0 @@
-#ifndef __DBGET_H__
-#define __DBGET_H__
-
-char *dbget(char *name, char *defval);
-
-#endif //__DBGET_H__
--- yavdr-utils/db-utils/dbset.h
+++
@@ -1,6 +0,0 @@
-#ifndef __DBSET_H__
-#define __DBSET_H__
-
-int dbset(const char *fmt, ...);
-
-#endif //__DBSET_H__
